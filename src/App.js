0122028c413e3a98741f440b3651f615e959ef44
Guilherme Focassio <guifocassio1@gmail.com>
2023-02-18 16:28:23 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <img src={process.env.PUBLIC_URL + '/images/image-qr-code.png'} alt="QR Code" />
+      <h1>Improve your front-end skills by building projects</h1>
+      <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    </div>
+  );
+}
+
+export default App;
